dc4b6fb4328b49dcb7a7343f3be283be55e18d55
Pedro Galveias <pedro.r.galveias@pm.me>
2021-06-25 14:23:03 +0200

    Updated topbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
-import Work from './components/work/Work';
+import Works from './components/work/Works';
 import Contact from './components/contact/Contact';
+import {useState} from "react";
 
 function App() {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="App">
-        <Topbar/>
+        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <div className="sections">
           <Intro/>
           <Portfolio/>
           <Testimonials/>
-          <Work/>
+          <Works/>
           <Contact/>
         </div>
         <p>
