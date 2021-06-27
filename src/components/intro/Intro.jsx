3fb24948973d8429275846efd88ee25f2ab2f3e1
Pedro Galveias <pedro.r.galveias@pm.me>
2021-06-28 01:42:12 +0200

    Added assets, intro text & animations

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import './intro.scss'
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Software", "Blockchain", "Database"]
+        });
+    }, []);
+
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/me.png" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello there! I am</h2>
+                    <h1>Pedro Galveias</h1>
+                    <h3><span ref={ textRef }></span> Engineer</h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/arrow_down.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
