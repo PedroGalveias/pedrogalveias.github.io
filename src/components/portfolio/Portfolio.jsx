27fc7a2d6691a76ef32a649c6df1cedf48372d05
Pedro Galveias <pedro.r.galveias@pm.me>
2021-06-28 02:09:19 +0200

    Started portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,32 @@ import './portfolio.scss'
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Ethereum</li>
+                <li>Blockchain Dev</li>
+                <li>Bachelor's Project</li>
+                <li>Uni Projects</li>
+                <li>Other</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png" alt=""/>
+                    <h3>Blockchain</h3>
+                </div>
+                <div className="item">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png" alt=""/>
+                    <h3>Blockchain</h3>
+                </div>
+                <div className="item">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png" alt=""/>
+                    <h3>Blockchain</h3>
+                </div>
+                <div className="item">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png" alt=""/>
+                    <h3>Blockchain</h3>
+                </div>
+            </div>
         </div>
     )
 }
